70bb8d769367ce356587c8413b649411c3a21a2c
Jevin <jevanderson@paypal.com>
2020-02-07 15:25:03 -0800

    Initial Commit

---
+++ Carthage Test Framework/Carthage_Test_Framework.h
@@ -0,0 +1,19 @@
+//
+//  Carthage_Test_Framework.h
+//  Carthage Test Framework
+//
+//  Created by Anderson, Jevin on 2/7/20.
+//  Copyright © 2020 Anderson, Jevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Carthage_Test_Framework.
+FOUNDATION_EXPORT double Carthage_Test_FrameworkVersionNumber;
+
+//! Project version string for Carthage_Test_Framework.
+FOUNDATION_EXPORT const unsigned char Carthage_Test_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage_Test_Framework/PublicHeader.h>
+
+
