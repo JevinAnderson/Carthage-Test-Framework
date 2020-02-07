bf3cabe5c91f14496d84edd8ee9d6ca7013b478e
Jevin <jevanderson@paypal.com>
2020-02-07 15:30:10 -0800

    setup

---
+++ Sample app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample app
+//
+//  Created by Anderson, Jevin on 2/7/20.
+//  Copyright © 2020 Anderson, Jevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
